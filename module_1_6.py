3eb1b8a17ff5603dda24eb52bb3b0ddce2b459d3
andrey1968-dotcom <cushvintsev36@gmail.com>
2024-12-05 19:26:54 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,29 @@
+my_dict = {'Andrey' : 1998, 'Natasha' : 2003, 'Alex' : 1995, 'Mary' : 1987}
+print(my_dict)
+print(my_dict['Mary'])
+my_dict['Felix'] = 1991
+print(my_dict)
+my_dict['Andrey'] = 1996
+print(my_dict)
+my_dict['Pavel'] = 1978
+my_dict['Elena'] = 2003
+print(my_dict)
+del my_dict['Andrey']
+print(my_dict)
+print(my_dict.get('Pavel'))
+print(my_dict.get('Vera'))
+my_set = {0, 1, 2, 0, 6, 7, 3, 1, 'False', 'Boolean', 6.34, 6.34, True}
+print(my_set)
+print(my_set.add('Apple'))
+print(my_set)
+print(my_set.add('Fals'))
+print(my_set)
+print(my_set.discard(6.34))
+print(my_set)
+
+
+
+
+
+
+
